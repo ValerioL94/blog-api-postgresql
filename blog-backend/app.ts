6ae94da9a7ba9aace8454a02bfc2a33e9e4fd6ae
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-06 19:00:17 +0200

    import dependencies into app.ts

--- blog-backend/app.ts
+++ blog-backend/app.ts
@@ -1,11 +1,33 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import createError, { HttpError } from 'http-errors';
+import passport from 'passport';
+import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
 });
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
