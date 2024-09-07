ca891bfe8937b5cc4edf165a6a2ec88a1467853d
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-07 19:03:01 +0200

    add zod validation userSchema

---
+++ blog-backend/utils/zodSchema.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+import { prisma } from '../prisma/client';
+
+export const UserSchema = z
+  .object({
+    username: z
+      .string()
+      .trim()
+      .min(3, 'username must contain at least 3 characters')
+      .max(100, 'username must contain less than 100 characters'),
+    email: z
+      .string()
+      .email()
+      .refine(async (value) => {
+        const user = await prisma.user.findUnique({ where: { email: value } });
+        user ? false : true;
+      }, 'Email already in use'),
+    password: z
+      .string()
+      .regex(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+        'Password must contain at least 8 characters, 1 uppercase letter, 1 lowercase letter and 1 number'
+      ),
+    confirm: z.string(),
+  })
+  .refine((data) => data.password === data.confirm, {
+    message: "Passwords don't match",
+    path: ['confirm'],
+  });
