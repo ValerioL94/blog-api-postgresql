6293c6a02d0490abe2256f27b5f97bc76b0199a9
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-07 19:05:33 +0200

    add user signup/login controllers

--- blog-backend/controllers/userController.ts
+++ blog-backend/controllers/userController.ts
@@ -1,12 +1,55 @@
+import { fromZodError } from 'zod-validation-error';
 import asyncHandler from 'express-async-handler';
-import { body } from 'express-validator';
+import { UserSchema } from '../utils/zodSchema';
+import bcrypt from 'bcryptjs';
+import { prisma } from '../prisma/client';
+import jwt from 'jsonwebtoken';
+
+export const user_sign_up = asyncHandler(async (req, res, next) => {
+  const { body } = req;
+  const results = await UserSchema.safeParseAsync(body);
+  if (!results.success) {
+    const errors = fromZodError(results.error).details;
+    res.json({ errors: errors });
+  } else {
+    const data = results.data;
+    bcrypt.hash(data.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        return next(err);
+      } else {
+        await prisma.user.create({
+          data: {
+            username: data.username,
+            email: data.email,
+            password: hashedPassword,
+          },
+        });
+        return res.json({ message: 'Sign-up successful' });
+      }
+    });
+  }
+});
 
 export const user_log_in = asyncHandler(async (req, res, next) => {
-  res.json({ message: 'wip' });
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({ where: { email: email } });
+  if (!user) {
+    res.json({ errors: 'Email not found' });
+  } else {
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      res.json({ errors: 'Wrong password' });
+    } else {
+      const JWT_SECRET = process.env.SECRET!;
+      const token = jwt.sign({ email }, JWT_SECRET, { expiresIn: 600 });
+      res.json({
+        token: token,
+        user: {
+          id: user.id,
+          username: user.username,
+          email: user.email,
+        },
+      });
+    }
+  }
 });
-export const user_sign_up = [
-  body(),
-  asyncHandler(async (req, res, next) => {
-    res.json({ message: 'wip' });
-  }),
-];
--- blog-backend/routes/user.ts
+++ blog-backend/routes/user.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { user_sign_up, user_log_in } from '../controllers/userController';
+
 export const router = express.Router({ mergeParams: true });
 
 router.post('/signup', user_sign_up);
