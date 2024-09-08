22f3d570c66dbc69283c30dff31603b5cf7fa523
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-08 13:41:58 +0200

    add database option for testing

--- blog-backend/prisma/client.ts
+++ blog-backend/prisma/client.ts
@@ -1,3 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+const databaseUrl =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl,
+    },
+  },
+});
