e93c64c1f6a3aa21c07d17dae6e960f67a2ccc36
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-11 10:46:38 +0200

    update comment router

--- blog-backend/routes/comment.ts
+++ blog-backend/routes/comment.ts
@@ -1,18 +1,14 @@
 import express from 'express';
 import {
-  comment_list,
-  comment_detail,
   comment_create,
   comment_update,
   comment_delete,
 } from '../controllers/commentController';
-
+import passport from 'passport';
 export const router = express.Router({ mergeParams: true });
 
-router.get('/', comment_list);
+router.post('/', comment_create);
 router
   .route('/:commentId')
-  .get(comment_detail)
-  .post(comment_create)
-  .put(comment_update)
-  .delete(comment_delete);
+  .put(passport.authenticate('jwt', { session: false }), comment_update)
+  .delete(passport.authenticate('jwt', { session: false }), comment_delete);
