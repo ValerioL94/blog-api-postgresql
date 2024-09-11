dae533b51cc5b38bd9f1b669e7e3c7db41474fa6
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-11 16:59:09 +0200

    Initalize frontend-author

---
+++ blog-frontend-author/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex items-center justify-center'>
+      <h1 className='font-semibold text-green-700'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
