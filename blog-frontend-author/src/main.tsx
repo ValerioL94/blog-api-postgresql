868aab38467effaa3184dd145ec78f71a44c56e5
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-11 20:13:30 +0200

    add auth provider

--- blog-frontend-author/src/main.tsx
+++ blog-frontend-author/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import AuthProvider from './provider/Provider.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+);
