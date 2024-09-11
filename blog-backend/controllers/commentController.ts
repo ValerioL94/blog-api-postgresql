a0c6e9e028f607d454663a47245a191501b3bd5b
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-11 10:47:17 +0200

    add comment controller crud functions

--- blog-backend/controllers/commentController.ts
+++ blog-backend/controllers/commentController.ts
@@ -1,28 +1,41 @@
 import asyncHandler from 'express-async-handler';
-import { body } from 'express-validator';
+import { ICommentUpsertRequest } from '../types/types';
+import { CommentSchema } from '../utils/zodSchema';
+import { fromZodError } from 'zod-validation-error';
+import { prisma } from '../prisma/client';
 
-export const comment_list = asyncHandler(async (req, res, next) => {
-  res.json({ message: 'all comments' });
+export const comment_create = asyncHandler(async (req, res, next) => {
+  const { body }: ICommentUpsertRequest = req;
+  const results = await CommentSchema.safeParseAsync(body);
+  if (!results.success) {
+    const errors = fromZodError(results.error).details;
+    res.json({ errors });
+  } else {
+    const parsedData = results.data;
+    const comment = await prisma.comment.create({
+      data: { ...parsedData, postId: req.params.postId },
+    });
+  }
+  res.json({ message: 'Comment created successfully' });
 });
 
-export const comment_create = [
-  body(),
-  asyncHandler(async (req, res, next) => {
-    res.json({ message: 'create comment' });
-  }),
-];
-
-export const comment_detail = asyncHandler(async (req, res, next) => {
-  res.json({ message: 'single comment' });
+export const comment_update = asyncHandler(async (req, res, next) => {
+  const { body }: ICommentUpsertRequest = req;
+  const results = await CommentSchema.safeParseAsync(body);
+  if (!results.success) {
+    const errors = fromZodError(results.error).details;
+    res.json({ errors });
+  } else {
+    const parsedData = results.data;
+    await prisma.comment.update({
+      where: { id: req.params.commentId },
+      data: parsedData,
+    });
+  }
+  res.json({ message: 'Comment updated successfully' });
 });
-
-export const comment_update = [
-  body(),
-  asyncHandler(async (req, res, next) => {
-    res.json({ message: 'update comment' });
-  }),
-];
 
 export const comment_delete = asyncHandler(async (req, res, next) => {
-  res.json({ message: 'delete comment' });
+  await prisma.comment.delete({ where: { id: req.params.commentId } });
+  res.json({ message: 'Comment deleted successfully' });
 });
