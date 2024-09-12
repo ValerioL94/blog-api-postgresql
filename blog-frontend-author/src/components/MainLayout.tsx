b4add0b52bd5479d94040512a5f5dd10cf426d51
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-12 12:40:11 +0200

    add main layout styles

--- blog-frontend-author/src/components/MainLayout.tsx
+++ blog-frontend-author/src/components/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+// import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './Navbar';
 const MainLayout = () => {
@@ -8,20 +8,23 @@ const MainLayout = () => {
       <header className='flex justify-center text-white bg-green-700 font-semibold border-b-2 border-gray-500 sticky top-0 '>
         <Navbar />
       </header>
-      <main>
+      <main className='flex flex-col items-center h-full bg-white my-2 p-2 box-border'>
         <Outlet />
       </main>
-      <footer>
+      <footer className='w-full flex items-center justify-center gap-2 border-t-2 border-gray-500 bg-green-700 font text-white'>
         <p>Copyright © 2024 ValerioL94</p>
-        <a className='git-link' href='https://github.com/ValerioL94'>
+        <a
+          className='hover:animate-pulse focus:animate-pulse'
+          href='https://github.com/ValerioL94'
+        >
           <img
-            className='logo'
+            className='h-6'
             src='/assets/icons/github-mark-white.svg'
             alt='github logo'
           />
         </a>
       </footer>
-      <ToastContainer />
+      {/* <ToastContainer /> */}
     </>
   );
 };
