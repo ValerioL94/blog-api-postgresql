decbe2f160d6ea0bb0c2b9ff716a4aa3631f28d6
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-12 12:51:12 +0200

    add route to about page

--- blog-frontend-author/src/App.tsx
+++ blog-frontend-author/src/App.tsx
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
 import HomePage from './pages/HomePage';
+import About from './pages/About';
 
 function App() {
   const router = createBrowserRouter(
@@ -15,6 +16,7 @@ function App() {
         <Route path='/' element={<MainLayout />}>
           <Route index loader={async () => redirect('home')} />
           <Route path='home' element={<HomePage />} />
+          <Route path='about' element={<About />} />
         </Route>
       </>
     )
