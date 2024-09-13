70fc00c2f82c5466189d7ebdaf110d76295e35c5
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-13 11:19:03 +0200

    update types

--- blog-frontend-author/src/types/types.ts
+++ blog-frontend-author/src/types/types.ts
@@ -1,4 +1,14 @@
-export type TokenContext = {
-  token: string | null;
-  setToken: React.Dispatch<React.SetStateAction<string | null>>;
+export type TUser = {
+  id: string;
+  username: string;
+  email: string;
+};
+
+export type TAuthData = {
+  token: string;
+  user: TUser;
+} | null;
+export type TAuthContext = {
+  authData: TAuthData;
+  setAuthData: React.Dispatch<React.SetStateAction<TAuthData>>;
 };
