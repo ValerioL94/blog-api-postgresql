cd70d53dad4783689a067348e86d6d49db3129d9
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-13 11:44:25 +0200

    update styles

--- blog-frontend-author/src/components/MainLayout.tsx
+++ blog-frontend-author/src/components/MainLayout.tsx
@@ -8,7 +8,7 @@ const MainLayout = () => {
       <header className='flex justify-center text-white bg-green-700 font-semibold border-b-2 border-gray-500 sticky top-0 '>
         <Navbar />
       </header>
-      <main className='flex flex-col items-center h-full bg-white my-2 p-2 box-border'>
+      <main className='flex flex-col items-center h-full bg-green-50 p-4 box-border'>
         <Outlet />
       </main>
       <footer className='w-full flex items-center justify-center gap-2 border-t-2 border-gray-500 bg-green-700 font text-white'>
--- blog-frontend-author/src/pages/About.tsx
+++ blog-frontend-author/src/pages/About.tsx
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div className='flex flex-col p-1 w-full max-w-3xl rounded shadow-md shadow-gray-500'>
+    <div className='flex flex-col gap-2 p-4 w-full max-w-3xl rounded bg-white shadow-md shadow-gray-500'>
       <h1 className='text-center text-green-700 font-bold text-xl'>About</h1>
       <p>
         This is the front-end site of the blog-API project where registered
