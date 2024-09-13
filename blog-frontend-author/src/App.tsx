2a408f0e0eb7fd834b3a530b0ff883aca483a28b
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-13 19:08:04 +0200

    add signup/login route

--- blog-frontend-author/src/App.tsx
+++ blog-frontend-author/src/App.tsx
@@ -8,6 +8,8 @@ import {
 import MainLayout from './components/MainLayout';
 import HomePage from './pages/HomePage';
 import About from './pages/About';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   const router = createBrowserRouter(
@@ -17,6 +19,8 @@ function App() {
           <Route index loader={async () => redirect('home')} />
           <Route path='home' element={<HomePage />} />
           <Route path='about' element={<About />} />
+          <Route path='signup' element={<Signup />} />
+          <Route path='login' element={<Login />} />
         </Route>
       </>
     )
