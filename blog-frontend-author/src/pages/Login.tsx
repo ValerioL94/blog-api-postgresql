e0fc52c5cb8c6e3491242868965c41447f2e01a8
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-14 20:45:41 +0200

    update login page

--- blog-frontend-author/src/pages/Login.tsx
+++ blog-frontend-author/src/pages/Login.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
 import LoginForm from '../components/LoginForm';
 import { useAuth } from '../provider/context';
 
 const Login = () => {
-  const [errors, setErrors] = useState<string[] | null>(null);
   const { authData } = useAuth();
+
   return (
     <div className='flex flex-col gap-2 p-4 w-full max-w-3xl rounded bg-white shadow-md shadow-gray-500'>
       {authData ? (
@@ -19,17 +18,6 @@ const Login = () => {
           <div className='p-2'>
             <LoginForm />
           </div>
-          {errors ? (
-            <ul>
-              {errors.map((error, index) => (
-                <li className='errors-list' key={index}>
-                  {error}
-                </li>
-              ))}
-            </ul>
-          ) : (
-            ''
-          )}
         </>
       )}
     </div>
