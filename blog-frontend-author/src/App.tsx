2b9b6ef4ef13b3b7e10503dc429d8e440b23cd97
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-14 20:47:13 +0200

    update routes

--- blog-frontend-author/src/App.tsx
+++ blog-frontend-author/src/App.tsx
@@ -10,17 +10,27 @@ import HomePage from './pages/HomePage';
 import About from './pages/About';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import ErrorPage from './pages/ErrorPage';
+import { userAction } from './actions/userActions';
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path='/' element={<MainLayout />}>
+        <Route path='/' errorElement={<ErrorPage />} element={<MainLayout />}>
           <Route index loader={async () => redirect('home')} />
           <Route path='home' element={<HomePage />} />
           <Route path='about' element={<About />} />
-          <Route path='signup' element={<Signup />} />
-          <Route path='login' element={<Login />} />
+          <Route
+            path='signup'
+            action={async ({ request }) => await userAction(request, 'signup')}
+            element={<Signup />}
+          />
+          <Route
+            path='login'
+            action={async ({ request }) => await userAction(request, 'login')}
+            element={<Login />}
+          />
         </Route>
       </>
     )
