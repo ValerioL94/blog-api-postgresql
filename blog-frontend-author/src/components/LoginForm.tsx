21c014d2f4f1aa935d3ee4d66b51852c9350e81c
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-14 17:12:55 +0200

    update login form

--- blog-frontend-author/src/components/LoginForm.tsx
+++ blog-frontend-author/src/components/LoginForm.tsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { Form } from 'react-router-dom';
 
 const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <Form className='p-1' method='POST'>
       <label htmlFor='email'>Email</label>
@@ -9,16 +14,31 @@ const LoginForm = () => {
         type='email'
         name='email'
         id='email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
         placeholder='jake@gmail.com'
         autoComplete='email'
         required
       />
-      <label htmlFor='password'>Password</label>
+      <label htmlFor='password'>Password</label>{' '}
+      {showPassword ? (
+        <FaEye
+          className='cursor-pointer'
+          onClick={() => setShowPassword(false)}
+        />
+      ) : (
+        <FaEyeSlash
+          className='cursor-pointer'
+          onClick={() => setShowPassword(true)}
+        />
+      )}
       <input
         className='block w-full py-1 px-2 mb-3 border-2 border-solid border-gray-500 rounded-md text-sm hover:border-green-600 focus:border-green-700 outline-none  '
-        type='password'
+        type={showPassword ? 'text' : 'password'}
         name='password'
         id='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
         autoComplete='current-password'
         required
       />
