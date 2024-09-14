7379321c6c59663beb1307b2e8a537d961911bc8
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-14 20:46:26 +0200

    update login form component

--- blog-frontend-author/src/components/LoginForm.tsx
+++ blog-frontend-author/src/components/LoginForm.tsx
@@ -1,54 +1,87 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import { Form } from 'react-router-dom';
+import { Form, useActionData, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import ErrorList from './ErrorList';
 
+const initialState = {
+  email: '',
+  password: '',
+};
 const LoginForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [formState, setFormState] = useState(initialState);
   const [showPassword, setShowPassword] = useState(false);
+  const [errors, setErrors] = useState(null);
+  const response = useActionData();
+  const navigate = useNavigate();
+  function handleChange(e) {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
+  }
+  useEffect(() => {
+    if (response && response.errors) {
+      setErrors(response.errors);
+    }
+    if (response && !response.errors) {
+      setFormState(initialState);
+      setErrors(null);
+      toast.success('Signup successful!', { autoClose: 3000 });
+      setTimeout(() => {
+        navigate('/home', { replace: true });
+      }, 3000);
+    }
+  }, [response, navigate]);
+
   return (
-    <Form className='p-1' method='POST'>
-      <label htmlFor='email'>Email</label>
-      <input
-        className='block w-full py-1 px-2 mb-3 border-2 border-solid border-gray-500 rounded-md text-sm hover:border-green-600 focus:border-green-700 outline-none  '
-        type='email'
-        name='email'
-        id='email'
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder='jake@gmail.com'
-        autoComplete='email'
-        required
-      />
-      <label htmlFor='password'>Password</label>{' '}
-      {showPassword ? (
-        <FaEye
-          className='cursor-pointer'
-          onClick={() => setShowPassword(false)}
+    <>
+      <Form className='p-1' method='POST'>
+        <label htmlFor='email'>Email</label>
+        <input
+          className='block w-full py-1 px-2 mb-3 border-2 border-solid border-gray-500 rounded-md text-sm hover:border-green-600 focus:border-green-700 outline-none  '
+          type='email'
+          name='email'
+          id='email'
+          value={formState.email}
+          onChange={handleChange}
+          placeholder='jake@gmail.com'
+          autoComplete='email'
+          required
         />
-      ) : (
-        <FaEyeSlash
-          className='cursor-pointer'
-          onClick={() => setShowPassword(true)}
+        <div className='flex items-center gap-4'>
+          <label htmlFor='password'>Password</label>
+          {showPassword ? (
+            <FaEye
+              className='cursor-pointer'
+              onClick={() => setShowPassword(false)}
+            />
+          ) : (
+            <FaEyeSlash
+              className='cursor-pointer'
+              onClick={() => setShowPassword(true)}
+            />
+          )}
+        </div>
+        <input
+          className='block w-full py-1 px-2 mb-3 border-2 border-solid border-gray-500 rounded-md text-sm hover:border-green-600 focus:border-green-700 outline-none  '
+          type={showPassword ? 'text' : 'password'}
+          name='password'
+          id='password'
+          value={formState.password}
+          onChange={handleChange}
+          autoComplete='current-password'
+          required
         />
-      )}
-      <input
-        className='block w-full py-1 px-2 mb-3 border-2 border-solid border-gray-500 rounded-md text-sm hover:border-green-600 focus:border-green-700 outline-none  '
-        type={showPassword ? 'text' : 'password'}
-        name='password'
-        id='password'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        autoComplete='current-password'
-        required
-      />
-      <button
-        className='h-7 min-w-20 border-2 border-gray-500 border-solid rounded-md text-sm font-semibold cursor-pointer text-green-700 bg-white hover:bg-green-600 hover:text-white  focus:bg-green-700 focus:text-white'
-        type='submit'
-      >
-        Submit
-      </button>
-    </Form>
+        <button
+          className='h-7 min-w-20 border-2 border-gray-500 border-solid rounded-md text-sm font-semibold cursor-pointer text-green-700 bg-white hover:bg-green-600 hover:text-white  focus:bg-green-700 focus:text-white'
+          type='submit'
+        >
+          Submit
+        </button>
+      </Form>
+      {errors ? <ErrorList errors={errors} /> : ''}
+    </>
   );
 };
 
