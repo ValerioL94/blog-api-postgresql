c03cbe048748cc1103c12823e6b833f9c569deaf
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-14 20:45:31 +0200

    update signup page

--- blog-frontend-author/src/pages/Signup.tsx
+++ blog-frontend-author/src/pages/Signup.tsx
@@ -1,13 +1,7 @@
-// import { useActionData, useNavigate } from 'react-router-dom';
-
-import { useState } from 'react';
 import SignupForm from '../components/SignupForm';
 import { useAuth } from '../provider/context';
 
 const Signup = () => {
-  // const response = useActionData()
-  // const navigate = useNavigate()
-  const [errors, setErrors] = useState<string[] | null>(null);
   const { authData } = useAuth();
   return (
     <div className='flex flex-col gap-2 p-4 w-full max-w-3xl rounded bg-white shadow-md shadow-gray-500'>
@@ -23,17 +17,6 @@ const Signup = () => {
           <div className='p-2'>
             <SignupForm />
           </div>
-          {errors ? (
-            <ul>
-              {errors.map((error, index) => (
-                <li className='errors-list' key={index}>
-                  {error}
-                </li>
-              ))}
-            </ul>
-          ) : (
-            ''
-          )}
         </>
       )}
     </div>
