f21cc2a08ebdfbd369750d33f8d6db499d205eed
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-14 16:51:23 +0200

    add signup form /reducer types

--- blog-frontend-author/src/types/types.ts
+++ blog-frontend-author/src/types/types.ts
@@ -13,3 +13,20 @@ export type TAuthContext = {
   authData: TAuthData;
   setAuthData: React.Dispatch<React.SetStateAction<TAuthData>>;
 };
+
+export type TSignupForm = {
+  username: string;
+  email: string;
+  password: string;
+  confirm: string;
+  authorKey: string;
+  showPassword: boolean;
+  showConfirm: boolean;
+  showAuthorKey: boolean;
+};
+
+export type SignupReducerAction = {
+  type: string;
+  field?: string;
+  payload?: string;
+};
