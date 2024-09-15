31dae4aba0054354487f2956a6f7584bb1fae811
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-15 11:57:50 +0200

    update signup page

--- blog-frontend-author/src/pages/Signup.tsx
+++ blog-frontend-author/src/pages/Signup.tsx
@@ -1,24 +1,12 @@
 import SignupForm from '../components/SignupForm';
-import { useAuth } from '../provider/context';
 
 const Signup = () => {
-  const { authData } = useAuth();
   return (
     <div className='flex flex-col gap-2 p-4 w-full max-w-3xl rounded bg-white shadow-md shadow-gray-500'>
-      {authData ? (
-        <h1 className='text-center text-green-700 font-bold text-xl'>
-          Welcome back {authData.user.username}
-        </h1>
-      ) : (
-        <>
-          <h1 className='text-center text-green-700 font-bold text-xl'>
-            Sign-up
-          </h1>
-          <div className='p-2'>
-            <SignupForm />
-          </div>
-        </>
-      )}
+      <h1 className='text-center text-green-700 font-bold text-xl'>Sign-up</h1>
+      <div className='p-2'>
+        <SignupForm />
+      </div>
     </div>
   );
 };
