6f424787bf4c6d0f7890ed34347ea860c624b51d
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-15 11:57:33 +0200

    update login form

--- blog-frontend-author/src/pages/Login.tsx
+++ blog-frontend-author/src/pages/Login.tsx
@@ -1,25 +1,12 @@
 import LoginForm from '../components/LoginForm';
-import { useAuth } from '../provider/context';
 
 const Login = () => {
-  const { authData } = useAuth();
-
   return (
     <div className='flex flex-col gap-2 p-4 w-full max-w-3xl rounded bg-white shadow-md shadow-gray-500'>
-      {authData ? (
-        <h1 className='text-center text-green-700 font-bold text-xl'>
-          Welcome back {authData.user.username}
-        </h1>
-      ) : (
-        <>
-          <h1 className='text-center text-green-700 font-bold text-xl'>
-            Log-in
-          </h1>
-          <div className='p-2'>
-            <LoginForm />
-          </div>
-        </>
-      )}
+      <h1 className='text-center text-green-700 font-bold text-xl'>Log-in</h1>
+      <div className='p-2'>
+        <LoginForm />
+      </div>
     </div>
   );
 };
