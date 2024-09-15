78d21e222bd216d0790b0cd8fe6646817878a603
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-15 12:16:46 +0200

    add types

--- blog-frontend-author/src/actions/userActions.ts
+++ blog-frontend-author/src/actions/userActions.ts
@@ -1,4 +1,9 @@
-async function postRequest(url, data) {
+async function postRequest(
+  url: string,
+  data: {
+    [k: string]: FormDataEntryValue;
+  }
+) {
   const response = await fetch(url, {
     method: 'post',
     headers: {
@@ -12,7 +17,7 @@ async function postRequest(url, data) {
   return response.json();
 }
 
-export async function userAction(request, path) {
+export async function userAction(request: Request, path: string) {
   const endpoint = `/api/users/${path}`;
   const formData = await request.formData();
   const payload = Object.fromEntries(formData.entries());
