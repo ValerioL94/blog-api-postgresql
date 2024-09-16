fb13c0df90ad5f0a8ab82e1a763a13ac388e8e97
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-16 18:12:41 +0200

    update types

--- blog-frontend-author/src/types/types.ts
+++ blog-frontend-author/src/types/types.ts
@@ -35,3 +35,46 @@ export type TValidationError = {
   message: string;
 };
 export type TValidationErrors = TValidationError[];
+
+export type PostPreview = Omit<PostDetail, 'comments'>;
+
+export type PostList = PostPreview[];
+
+export type PostCreate = {
+  title: string;
+  content: string;
+  published: boolean;
+  authorId: string;
+};
+export type PostUpdate = Omit<PostCreate, 'authorId'>;
+
+export type PostDetail = {
+  id: string;
+  title: string;
+  content: string;
+  published: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  authorId: string;
+  author: {
+    username: string;
+    email: string;
+  };
+  comments?: CommentList;
+};
+
+export type CommentDetail = {
+  id: string;
+  username: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  postId: string;
+};
+
+export type CommentList = CommentDetail[];
+
+export type CommentUpsert = {
+  content: string;
+  username: string;
+};
