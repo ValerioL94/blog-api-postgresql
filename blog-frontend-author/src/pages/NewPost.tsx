cf66823146c0d8a8c5610c9690a87fce6da9ba55
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-17 18:56:34 +0200

    add new post page

---
+++ blog-frontend-author/src/pages/NewPost.tsx
@@ -0,0 +1,14 @@
+import PostForm from '../components/PostForm';
+
+const NewPost = () => {
+  return (
+    <div className='flex flex-col gap-2 p-4 w-full max-w-3xl rounded bg-white shadow-md shadow-gray-500'>
+      <h1 className='text-center text-green-700 font-bold text-xl'>Sign-up</h1>
+      <div className='p-2'>
+        <PostForm />
+      </div>
+    </div>
+  );
+};
+
+export default NewPost;
