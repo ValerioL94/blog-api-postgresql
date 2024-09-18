2f7c56d6e3762e169d82511d9c3aba51750f7eb3
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-18 13:02:23 +0200

    update post list page

--- blog-frontend-author/src/pages/Posts.tsx
+++ blog-frontend-author/src/pages/Posts.tsx
@@ -1,21 +1,21 @@
 import { Link, useLoaderData } from 'react-router-dom';
-import { PostList } from '../types/types';
-import Post from '../components/Post';
+import { TPostList } from '../types/types';
+import PostPreview from '../components/PostPreview';
 
 const Posts = () => {
-  const posts = useLoaderData() as PostList;
-
+  const { posts } = useLoaderData() as { posts: TPostList };
   return (
     <div className='flex flex-col gap-2 p-4 w-full max-w-3xl rounded bg-white shadow-md shadow-gray-500'>
       <h1 className='text-center text-green-700 font-bold text-xl'>Posts</h1>
       <div className='flex flex-col gap-3 p-1 box-border w-full'>
         {!posts && <h2>Fetching posts...</h2>}
-        {posts && posts.length ? (
-          posts.map((post) => <Post postData={post} />)
+        {posts && posts.length > 0 ? (
+          posts.map((post) => <PostPreview key={post.id} postData={post} />)
         ) : (
           <h2>There are no posts.</h2>
         )}
       </div>
+      <hr />
       <div className='flex items-center justify-between m-2 font-semibold'>
         <Link to={'new-post'}>New Post</Link>
         <button
