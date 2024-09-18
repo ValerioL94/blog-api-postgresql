4cfe00663ccf96d5ba00a8dd7a8417702eb2f423
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-18 13:02:10 +0200

    add new post form

---
+++ blog-frontend-author/src/components/PostForm.tsx
@@ -0,0 +1,159 @@
+import { Form, useActionData, useNavigate, useSubmit } from 'react-router-dom';
+import { useAuth } from '../provider/context';
+import { useEffect, useMemo, useState } from 'react';
+import { TPostCreate, TValidationErrors } from '../types/types';
+import { Editor } from '@tinymce/tinymce-react';
+import ErrorList from './ErrorList';
+import { toast } from 'react-toastify';
+
+const PostForm = () => {
+  const { authData } = useAuth();
+  const initialFormState: TPostCreate = useMemo(
+    () => ({
+      title: '',
+      content: '',
+      published: 'false',
+      authorId: authData!.user.id,
+    }),
+    [authData]
+  );
+  const navigate = useNavigate();
+  const response = useActionData();
+  const submit = useSubmit();
+  const [formData, setFormData] = useState(initialFormState);
+  const [formErrors, setFormErrors] = useState<TValidationErrors | null>(null);
+  function handleChange(
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
+  const parseEditorData = (content: string, editor) => {
+    const { targetElm } = editor;
+    const { name } = targetElm;
+    return {
+      target: {
+        name,
+        value: content,
+      },
+    };
+  };
+
+  useEffect(() => {
+    if (response) {
+      const { errors } = response as { errors: TValidationErrors };
+      if (errors) {
+        return setFormErrors(errors);
+      } else {
+        setFormData(initialFormState);
+        setFormErrors(null);
+        toast.success('Post created!', { autoClose: 2000 });
+        navigate('/posts', { replace: true });
+      }
+    }
+  }, [response, initialFormState, navigate]);
+
+  return (
+    <>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          submit(formData, { method: 'POST' });
+        }}
+        className='p-1'
+        method='POST'
+      >
+        <label htmlFor='title'>Title: </label>
+        <input
+          type='text'
+          name='title'
+          id='title'
+          className='block w-full py-1 px-2 mb-3 border-2 border-solid border-gray-500 rounded-md text-sm hover:border-green-600 focus:border-green-700 outline-none'
+          value={formData.title}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor='content'>Content: </label>
+        <Editor
+          apiKey='e0p67za2rxewpb4t1ff4jwma3opsrmuv2u1cihj7te5jpvx8'
+          textareaName='content'
+          id='content'
+          value={formData.content}
+          onEditorChange={(content, editor) =>
+            handleChange(parseEditorData(content, editor))
+          }
+          init={{
+            menubar: false,
+            plugins: [
+              // Core editing features
+              'anchor',
+              'autolink',
+              'charmap',
+              'codesample',
+              'emoticons',
+              'image',
+              'link',
+              'lists',
+              'media',
+              'searchreplace',
+              'table',
+              'visualblocks',
+              'wordcount',
+              // Your account includes a free trial of TinyMCE premium features
+              // Try the most popular premium features until Sep 30, 2024:
+              'checklist',
+              'mediaembed',
+              'casechange',
+              'export',
+              'formatpainter',
+              'pageembed',
+              'a11ychecker',
+              'tinymcespellchecker',
+              'permanentpen',
+              'powerpaste',
+              'advtable',
+              'advcode',
+              'editimage',
+              'advtemplate',
+              'mentions',
+              'tableofcontents',
+              'footnotes',
+              'autocorrect',
+              'typography',
+              'inlinecss',
+              'markdown',
+            ],
+            toolbar:
+              'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | removeformat',
+          }}
+        />
+        <label htmlFor='published'>Published: </label>
+        <select
+          name='published'
+          id='published'
+          className='block w-full py-1 px-2 mb-3 border-2 border-solid border-gray-500 rounded-md text-sm hover:border-green-600 focus:border-green-700 outline-none'
+          value={formData.published}
+          onChange={handleChange}
+          required
+        >
+          <option value='false'>No</option>
+          <option value='true'>Yes</option>
+        </select>
+        <button
+          className='h-7 min-w-20 border-2 border-gray-500 border-solid rounded-md text-sm font-semibold cursor-pointer text-green-700 bg-white hover:bg-green-600 hover:text-white  focus:bg-green-700 focus:text-white'
+          type='submit'
+        >
+          Submit
+        </button>
+      </Form>
+      {formErrors ? <ErrorList errors={formErrors} /> : ''}
+    </>
+  );
+};
+
+export default PostForm;
