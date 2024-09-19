d0da508d9c77edb1b9737137e1e307fac14381f0
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-19 19:11:52 +0200

    add smaller components for header, main and footer sections

---
+++ blog-frontend-author/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Navbar from './Navbar';
+
+const Header = () => {
+  return (
+    <header className='flex justify-center text-white bg-green-700 font-semibold border-b-2 border-gray-500 sticky top-0 '>
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
---
+++ blog-frontend-author/src/components/MainSection.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+const MainSection = () => {
+  return (
+    <main className='flex flex-col items-center h-full bg-green-50 p-4 box-border'>
+      <Outlet />
+      <ToastContainer />
+    </main>
+  );
+};
+
+export default MainSection;
