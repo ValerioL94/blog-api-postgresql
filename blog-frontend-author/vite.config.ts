737584c6c32fa5399d57bc9d5dcc3a9072c70d63
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-19 21:01:10 +0200

    add test configs

--- blog-frontend-author/vite.config.ts
+++ blog-frontend-author/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -11,5 +14,10 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './__tests__/setup.ts',
   },
 });
