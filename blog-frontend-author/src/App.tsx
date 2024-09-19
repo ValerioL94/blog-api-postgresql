56d0e4591543f5ed7267b2c314904207fe39b6a6
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-19 21:01:40 +0200

    update imports

--- blog-frontend-author/src/App.tsx
+++ blog-frontend-author/src/App.tsx
@@ -6,66 +6,64 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
-import GlobalLayout from './components/GlobalLayout';
-import HomePage from './pages/HomePage';
 import About from './pages/About';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
 import ErrorPage from './pages/ErrorPage';
-import { userAction } from './actions/userActions';
+import HomePage from './pages/HomePage';
+import Login from './pages/Login';
 import Logout from './pages/Logout';
-import { useAuth } from './provider/context';
-import Posts from './pages/Posts';
-import { postLoader, postsLoader } from './loaders/postLoader';
 import NewPost from './pages/NewPost';
+import PostDetail from './pages/Post';
+import Posts from './pages/Posts';
 import ProtectedRoute from './pages/ProtectedRoute';
+import Signup from './pages/Signup';
+import GlobalLayout from './components/GlobalLayout';
+import { useAuth } from './provider/context';
+import { postLoader, postsLoader } from './loaders/postLoader';
+import { userAction } from './actions/userActions';
 import { postAction } from './actions/postActions';
-import PostDetail from './components/PostDetail';
 
 const App = () => {
   const { authData } = useAuth();
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <>
-        <Route path='/' errorElement={<ErrorPage />} element={<GlobalLayout />}>
-          <Route index loader={async () => redirect('home')} />
-          <Route path='home' element={<HomePage />} />
-          <Route path='about' element={<About />} />
+      <Route path='/' errorElement={<ErrorPage />} element={<GlobalLayout />}>
+        <Route index loader={async () => redirect('home')} />
+        <Route path='home' element={<HomePage />} />
+        <Route path='about' element={<About />} />
+        <Route
+          path='signup'
+          action={async ({ request }) => await userAction(request, 'signup')}
+          element={authData ? <Navigate replace to={'/home'} /> : <Signup />}
+        />
+        <Route
+          path='login'
+          action={async ({ request }) => await userAction(request, 'login')}
+          element={authData ? <Navigate replace to={'/home'} /> : <Login />}
+        />
+        <Route
+          path='logout'
+          element={!authData ? <Navigate replace to={'/home'} /> : <Logout />}
+        />
+        <Route path='posts' element={<ProtectedRoute />}>
           <Route
-            path='signup'
-            action={async ({ request }) => await userAction(request, 'signup')}
-            element={authData ? <Navigate replace to={'/home'} /> : <Signup />}
+            index
+            loader={async () => await postsLoader()}
+            element={<Posts />}
           />
           <Route
-            path='login'
-            action={async ({ request }) => await userAction(request, 'login')}
-            element={authData ? <Navigate replace to={'/home'} /> : <Login />}
+            path='new-post'
+            element={<NewPost />}
+            action={async ({ request, params }) =>
+              await postAction(request, params, authData!.token)
+            }
           />
           <Route
-            path='logout'
-            element={!authData ? <Navigate replace to={'/home'} /> : <Logout />}
+            path=':postId'
+            element={<PostDetail />}
+            loader={async ({ params }) => await postLoader(params)}
           />
-          <Route path='posts' element={<ProtectedRoute />}>
-            <Route
-              index
-              loader={async () => await postsLoader()}
-              element={<Posts />}
-            />
-            <Route
-              path='new-post'
-              element={<NewPost />}
-              action={async ({ request, params }) =>
-                await postAction(request, params, authData!.token)
-              }
-            />
-            <Route
-              path=':postId'
-              element={<PostDetail />}
-              loader={async ({ params }) => await postLoader(params)}
-            />
-          </Route>
         </Route>
-      </>
+      </Route>
     )
   );
   return <RouterProvider router={router} />;
