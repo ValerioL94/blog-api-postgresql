715c42fd34df1acc38dabd2fe0ea673e9eeecc1f
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-20 19:54:27 +0200

    add authentication check

--- blog-frontend-author/src/pages/Logout.tsx
+++ blog-frontend-author/src/pages/Logout.tsx
@@ -1,26 +1,27 @@
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import { useAuth } from '../provider/context';
-import { useEffect } from 'react';
 import Spinner from '../components/Spinner';
 
 const Logout = () => {
-  const { setAuthData } = useAuth();
+  const { authData, setAuthData } = useAuth();
   const navigate = useNavigate();
 
-  useEffect(() => {
+  if (authData) {
     setTimeout(() => {
       setAuthData(null);
       navigate('/home', { replace: true });
     }, 2000);
-  }, [setAuthData, navigate]);
-  return (
-    <div className='flex flex-col gap-2 p-4 w-full max-w-3xl rounded bg-white shadow-md shadow-gray-500'>
-      <h1 className='text-center text-green-700 font-bold text-xl'>
-        Logging out...
-      </h1>
-      <Spinner loading={true} />
-    </div>
-  );
+    return (
+      <>
+        <h1 className='text-center text-green-700 font-bold text-xl'>
+          Logging out...
+        </h1>
+        <Spinner loading={true} />
+      </>
+    );
+  } else {
+    return <Navigate to={'/home'} replace={true} />;
+  }
 };
 
 export default Logout;
