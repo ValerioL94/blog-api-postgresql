bdc07ae9057195b9418c32efadb9979918c19246
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-20 19:52:12 +0200

    Refactor app
    app now contains only a routes export to allow usage of different routers for testing

--- blog-frontend-author/src/App.tsx
+++ blog-frontend-author/src/App.tsx
@@ -1,11 +1,4 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Navigate,
-  redirect,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
+import { createRoutesFromElements, redirect, Route } from 'react-router-dom';
 import About from './pages/About';
 import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
@@ -17,56 +10,44 @@ import Posts from './pages/Posts';
 import ProtectedRoute from './pages/ProtectedRoute';
 import Signup from './pages/Signup';
 import GlobalLayout from './components/GlobalLayout';
-import { useAuth } from './provider/context';
 import { postLoader, postsLoader } from './loaders/postLoader';
 import { userAction } from './actions/userActions';
 import { postAction } from './actions/postActions';
 
-const App = () => {
-  const { authData } = useAuth();
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' errorElement={<ErrorPage />} element={<GlobalLayout />}>
-        <Route index loader={async () => redirect('home')} />
-        <Route path='home' element={<HomePage />} />
-        <Route path='about' element={<About />} />
-        <Route
-          path='signup'
-          action={async ({ request }) => await userAction(request, 'signup')}
-          element={authData ? <Navigate replace to={'/home'} /> : <Signup />}
-        />
-        <Route
-          path='login'
-          action={async ({ request }) => await userAction(request, 'login')}
-          element={authData ? <Navigate replace to={'/home'} /> : <Login />}
-        />
-        <Route
-          path='logout'
-          element={!authData ? <Navigate replace to={'/home'} /> : <Logout />}
-        />
-        <Route path='posts' element={<ProtectedRoute />}>
-          <Route
-            index
-            loader={async () => await postsLoader()}
-            element={<Posts />}
-          />
-          <Route
-            path='new-post'
-            element={<NewPost />}
-            action={async ({ request, params }) =>
-              await postAction(request, params, authData!.token)
-            }
-          />
-          <Route
-            path=':postId'
-            element={<PostDetail />}
-            loader={async ({ params }) => await postLoader(params)}
-          />
-        </Route>
-      </Route>
-    )
-  );
-  return <RouterProvider router={router} />;
-};
-
-export default App;
+export const routes = createRoutesFromElements(
+  <Route path='/' errorElement={<ErrorPage />} element={<GlobalLayout />}>
+    <Route index loader={() => redirect('home')} />
+    <Route path='home' element={<HomePage />} />
+    <Route path='about' element={<About />} />
+    <Route
+      path='signup'
+      action={async ({ request }) => await userAction(request, 'signup')}
+      element={<Signup />}
+    />
+    <Route
+      path='login'
+      action={async ({ request }) => await userAction(request, 'login')}
+      element={<Login />}
+    />
+    <Route path='logout' element={<Logout />} />
+    <Route path='posts' element={<ProtectedRoute />}>
+      <Route
+        index
+        loader={async () => await postsLoader()}
+        element={<Posts />}
+      />
+      <Route
+        path='new-post'
+        element={<NewPost />}
+        action={async ({ request, params }) =>
+          await postAction(request, params)
+        }
+      />
+      <Route
+        path=':postId'
+        element={<PostDetail />}
+        loader={async ({ params }) => await postLoader(params)}
+      />
+    </Route>
+  </Route>
+);
