598adbe2a1bc28bce63761dc57ec0e7740d52763
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-20 19:54:17 +0200

    add authentication check

--- blog-frontend-author/src/pages/Login.tsx
+++ blog-frontend-author/src/pages/Login.tsx
@@ -1,11 +1,14 @@
+import { Navigate } from 'react-router-dom';
 import LoginForm from '../components/LoginForm';
+import { useAuth } from '../provider/context';
 
 const Login = () => {
+  const { authData } = useAuth();
   return (
     <div className='flex flex-col gap-2 p-4 w-full max-w-3xl rounded bg-white shadow-md shadow-gray-500'>
-      <h1 className='text-center text-green-700 font-bold text-xl'>Log-in</h1>
+      <h1 className='text-center text-green-700 font-bold text-xl'>Login</h1>
       <div className='p-2'>
-        <LoginForm />
+        {authData ? <Navigate to={'/home'} replace={true} /> : <LoginForm />}
       </div>
     </div>
   );
