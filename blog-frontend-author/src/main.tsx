74abf5fa8ad0e84e07a32e6986c0f16e27e167ca
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-20 19:52:43 +0200

    add router provider

--- blog-frontend-author/src/main.tsx
+++ blog-frontend-author/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { routes } from './App.tsx';
 import AuthProvider from './provider/Provider.tsx';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <RouterProvider router={router} />
     </AuthProvider>
   </StrictMode>
 );
