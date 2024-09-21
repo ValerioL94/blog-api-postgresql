5ab05e5c410aaeabd12171222b00b2dc6232ba14
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-21 19:53:26 +0200

    signup rendering tests

---
+++ blog-frontend-author/__tests__/pages/Signup.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { routes } from '../../src/App';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+
+import AuthProvider from '../../src/provider/Provider';
+import { AuthContext } from '../../src/provider/context';
+import { TAuthContext } from '../../src/types/types';
+
+describe('signup conditional rendering', () => {
+  const router = createMemoryRouter(routes, { initialEntries: ['/signup'] });
+  test('render signup page if user is not logged in', () => {
+    render(
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    );
+    expect(
+      screen.getByRole('heading', { name: /signup/i })
+    ).toBeInTheDocument();
+  });
+  test('render homepage if user is logged in', () => {
+    const testContext: TAuthContext = {
+      authData: {
+        token: 'test',
+        user: { id: 'test', username: 'testUser', email: 'test@gmail.com' },
+      },
+      setAuthData: () => {},
+    };
+    render(
+      <AuthContext.Provider value={testContext}>
+        <RouterProvider router={router} />
+      </AuthContext.Provider>
+    );
+    expect(
+      screen.getByRole('heading', { name: /homepage/i })
+    ).toBeInTheDocument();
+  });
+});
