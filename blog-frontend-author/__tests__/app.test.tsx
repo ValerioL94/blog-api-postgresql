c6442c7ab43cc46a955ac883df6661cebd8fa2ee
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-21 10:29:24 +0200

    add app tests

--- blog-frontend-author/__tests__/app.test.tsx
+++ blog-frontend-author/__tests__/app.test.tsx
@@ -6,21 +6,31 @@ import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 describe('routes rendering/navigating', () => {
   test('correct layout rendering', async () => {
     const router = createMemoryRouter(routes, { initialEntries: ['/'] });
+
     render(<RouterProvider router={router} />);
+
     const banner = await screen.findByRole('banner');
     const main = await screen.findByRole('main');
     const contentinfo = await screen.findByRole('contentinfo');
-
     expect(banner).toBeInTheDocument();
     expect(main).toBeInTheDocument();
     expect(contentinfo).toBeInTheDocument();
   });
+  test('home page rendering', () => {
+    const router = createMemoryRouter(routes, { initialEntries: ['/home'] });
+
+    render(<RouterProvider router={router} />);
+
+    const homeHeading = screen.getByRole('heading');
+    expect(homeHeading).toHaveTextContent(/homepage/i);
+  });
   test('error page on nonexisting page', async () => {
     const badRoute = '/bad/lost';
     const router = createMemoryRouter(routes, { initialEntries: [badRoute] });
+
     render(<RouterProvider router={router} />);
 
-    const errorHeading = await screen.findByRole('heading');
+    const errorHeading = screen.getByRole('heading');
     expect(errorHeading).toHaveTextContent(/error/i);
   });
 });
