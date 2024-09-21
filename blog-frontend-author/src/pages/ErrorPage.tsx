4b703cd5166b1b5f5fe02a768ce6ba5ef02b2a83
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-21 11:10:10 +0200

    add accessible name to home link

--- blog-frontend-author/src/pages/ErrorPage.tsx
+++ blog-frontend-author/src/pages/ErrorPage.tsx
@@ -44,10 +44,13 @@ const ErrorPage = () => {
       </div>
       <div className='flex flex-col items-center gap-2'>
         {RootBoundary()}
-        <Link to='/'>Click this link to go to home page.</Link>
+        <Link to='/' aria-label='home-link'>
+          Click this link to go to home page
+        </Link>
         <img
           className='rounded-3xl border-solid border-green-700 border-2'
           src='https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdXl1N2F4NHlxdDBhYjlocnI5cmVvNWdicGFma2hmeTYwajV5eW83aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/n7K7ghLDxsqqRysHDx/giphy.gif'
+          alt='penguin dressed as a shark fishing'
         />
         <p>
           <a href='https://giphy.com/gifs/pudgypenguins-fish-fishing-pudgy-n7K7ghLDxsqqRysHDx'>
