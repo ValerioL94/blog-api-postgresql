08283814e13c486c72e0f211b8f185e6976ef769
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-21 19:43:28 +0200

    add homepage tests

---
+++ blog-frontend-author/__tests__/pages/HomePage.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { routes } from '../../src/App';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+
+import AuthProvider from '../../src/provider/Provider';
+import { AuthContext } from '../../src/provider/context';
+
+describe('homepage conditional rendering', () => {
+  const router = createMemoryRouter(routes, { initialEntries: ['/home'] });
+  test('user not authenticated welcome message', () => {
+    render(
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    );
+    expect(
+      screen.getByText(
+        'Welcome to the homepage! On this site registered authors can manage both posts and comments.'
+      )
+    ).toBeInTheDocument();
+  });
+  test('user authenticated message', () => {
+    const testContext = {
+      authData: {
+        token: 'test',
+        user: { id: 'test', username: 'testUser', email: 'test@gmail.com' },
+      },
+      setAuthData: () => {},
+    };
+    render(
+      <AuthContext.Provider value={testContext}>
+        <RouterProvider router={router} />
+      </AuthContext.Provider>
+    );
+    expect(screen.getByText('testUser')).toBeInTheDocument();
+  });
+});
