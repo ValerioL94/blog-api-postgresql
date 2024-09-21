81b7978ecdd5b762d2128025e861ede4d8ad7da0
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-21 19:22:30 +0200

    update forms

--- blog-frontend-author/src/pages/Login.tsx
+++ blog-frontend-author/src/pages/Login.tsx
@@ -5,12 +5,20 @@ import { useAuth } from '../provider/context';
 const Login = () => {
   const { authData } = useAuth();
   return (
-    <div className='flex flex-col gap-2 p-4 w-full max-w-3xl rounded bg-white shadow-md shadow-gray-500'>
-      <h1 className='text-center text-green-700 font-bold text-xl'>Login</h1>
-      <div className='p-2'>
-        {authData ? <Navigate to={'/home'} replace={true} /> : <LoginForm />}
-      </div>
-    </div>
+    <>
+      {authData ? (
+        <Navigate to={'/home'} replace={true} />
+      ) : (
+        <div className='flex flex-col gap-2 p-4 w-full max-w-3xl rounded bg-white shadow-md shadow-gray-500'>
+          <h1 className='text-center text-green-700 font-bold text-xl'>
+            Login
+          </h1>
+          <div className='p-2'>
+            <LoginForm />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- blog-frontend-author/src/pages/Signup.tsx
+++ blog-frontend-author/src/pages/Signup.tsx
@@ -5,12 +5,20 @@ import { useAuth } from '../provider/context';
 const Signup = () => {
   const { authData } = useAuth();
   return (
-    <div className='flex flex-col gap-2 p-4 w-full max-w-3xl rounded bg-white shadow-md shadow-gray-500'>
-      <h1 className='text-center text-green-700 font-bold text-xl'>Signup</h1>
-      <div className='p-2'>
-        {authData ? <Navigate to={'/home'} replace={true} /> : <SignupForm />}
-      </div>
-    </div>
+    <>
+      {authData ? (
+        <Navigate to={'/home'} replace={true} />
+      ) : (
+        <div className='flex flex-col gap-2 p-4 w-full max-w-3xl rounded bg-white shadow-md shadow-gray-500'>
+          <h1 className='text-center text-green-700 font-bold text-xl'>
+            Signup
+          </h1>
+          <div className='p-2'>
+            <SignupForm />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
