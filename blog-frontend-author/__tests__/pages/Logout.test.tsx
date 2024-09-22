18006e80f9a6da4e022ec8a7d05991f260215607
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-22 18:41:03 +0200

    update logout test

--- blog-frontend-author/__tests__/pages/Logout.test.tsx
+++ blog-frontend-author/__tests__/pages/Logout.test.tsx
@@ -9,13 +9,13 @@ import { TAuthContext } from '../../src/types/types';
 
 describe('signup conditional rendering', () => {
   const router = createMemoryRouter(routes, { initialEntries: ['/logout'] });
-  test('render logout page if user is logged in', () => {
+  test('render logout page if user is logged in, log him out and redirect to homepage', async () => {
     const testContext: TAuthContext = {
       authData: {
         token: 'test',
         user: { id: 'test', username: 'testUser', email: 'test@gmail.com' },
       },
-      setAuthData: () => {},
+      setAuthData: () => null,
     };
     render(
       <AuthContext.Provider value={testContext}>
@@ -24,6 +24,13 @@ describe('signup conditional rendering', () => {
     );
     expect(
       screen.getByRole('heading', { name: /logging out/i })
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByRole(
+        'heading',
+        { name: /homepage/i },
+        { timeout: 3000 }
+      )
     ).toBeInTheDocument();
   });
   test('render homepage if user is not logged in', () => {
