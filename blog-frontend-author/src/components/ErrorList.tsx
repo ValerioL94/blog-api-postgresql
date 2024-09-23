e22a7e3d56a04a2a900bfaf9010451fa1abbe084
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-23 11:19:12 +0200

    add aria-label to error list

--- blog-frontend-author/src/components/ErrorList.tsx
+++ blog-frontend-author/src/components/ErrorList.tsx
@@ -3,9 +3,13 @@ import { TValidationErrors } from '../types/types';
 const ErrorList = ({ errors }: { errors: TValidationErrors }) => {
   return (
     <>
-      <ul className='flex flex-col gap-2 list-disc p-4'>
+      <ul aria-label='errorList' className='flex flex-col gap-2 list-disc p-4'>
         {errors.map((error, index) => (
-          <li className='text-red-600 font-semibold' key={index}>
+          <li
+            aria-label='errorListItem'
+            className='text-red-600 font-semibold'
+            key={index}
+          >
             {error.message}
           </li>
         ))}
