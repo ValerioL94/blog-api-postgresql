09ac878886cb897652e5c538e717e02922fd4bab
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-23 19:54:05 +0200

    update post actions

--- blog-frontend-author/src/actions/postActions.ts
+++ blog-frontend-author/src/actions/postActions.ts
@@ -1,13 +1,12 @@
 import { Params } from 'react-router-dom';
+import { TPostCreate, TPostUpdate } from '../types/types';
 
 // authenticated post request
 async function authRequest(
   url: string,
   method: string,
-  data: {
-    [k: string]: FormDataEntryValue;
-  },
-  token: FormDataEntryValue
+  data: TPostCreate | TPostUpdate | { id: string },
+  token: string
 ) {
   const response = await fetch(url, {
     method,
@@ -23,33 +22,44 @@ async function authRequest(
   return response.json();
 }
 
-// returns api endpoint depending on request method
-function getEndpoint(method: string, id: string | undefined) {
-  if (method === 'POST') return '/api/posts';
-  if (id) {
-    if (method === 'PUT' || method === 'DELETE') return `/api/posts/${id}`;
-    else throw new Error(`${method} not allowed`);
-  } else {
-    throw new Error('Post id missing');
-  }
-}
-
 // send a POST / PUT / DELETE request to the backend
 export async function postAction(request: Request, params: Params) {
   const method = request.method;
   const formData = await request.formData();
   const payload = Object.fromEntries(formData.entries());
-  const postData = {
-    title: payload.title,
-    content: payload.content,
-    published: payload.published,
-    authorId: payload.authorId,
-  };
-  const token = payload.token;
-  const endpoint = getEndpoint(request.method, params.postId);
+  const token: string = payload.token.toString();
   try {
-    const response = await authRequest(endpoint, method, postData, token);
-    return response;
+    if (method === 'POST') {
+      const endpoint = '/api/posts';
+      const postData: TPostCreate = {
+        title: payload.title.toString(),
+        content: payload.content.toString(),
+        published: payload.published.toString(),
+        authorId: payload.authorId.toString(),
+      };
+      const response = await authRequest(endpoint, method, postData, token);
+      return response;
+    }
+    if (method === 'PUT') {
+      const endpoint = `/api/posts/${params.postId}`;
+      const postData: TPostUpdate = {
+        title: payload.title.toString(),
+        content: payload.content.toString(),
+        published: payload.published.toString(),
+      };
+      const response = await authRequest(endpoint, method, postData, token);
+      return response;
+    }
+    if (method === 'DELETE') {
+      const endpoint = `/api/posts/${params.postId}`;
+      const postData = {
+        id: payload.id.toString(),
+      };
+      const response = await authRequest(endpoint, method, postData, token);
+      return response;
+    } else {
+      throw new Error(`${method} not allowed`);
+    }
   } catch (error) {
     throw new Error((error as Error).message);
   }
