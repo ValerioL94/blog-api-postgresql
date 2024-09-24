b43444b17c22d4128970ba1d1737cf972f43309e
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-24 10:09:22 +0200

    add custom animation

--- blog-frontend-author/tailwind.config.js
+++ blog-frontend-author/tailwind.config.js
@@ -1,8 +1,20 @@
+import { transform } from 'typescript';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadein: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+      },
+      animation: {
+        'fade-in': 'fadein 0.5s ',
+      },
+    },
   },
   plugins: [],
 };
