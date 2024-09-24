6c0f4e2f1333fc9c145f2dd8bf5ea336051bfef1
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-24 12:15:07 +0200

    update imports

--- blog-frontend-author/src/pages/NewPost.tsx
+++ blog-frontend-author/src/pages/NewPost.tsx
@@ -1,11 +1,11 @@
-import PostForm from '../components/PostForm';
+import PostFormCreate from '../components/PostFormCreate';
 
 const NewPost = () => {
   return (
     <div className='flex flex-col gap-2 p-4 w-full max-w-3xl rounded bg-white shadow-md shadow-gray-500'>
       <h1 className='text-center text-green-700 font-bold text-xl'>New Post</h1>
       <div className='p-2'>
-        <PostForm />
+        <PostFormCreate />
       </div>
     </div>
   );
