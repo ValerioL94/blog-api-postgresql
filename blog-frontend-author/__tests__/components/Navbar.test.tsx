3aa872f10cca3099782c730fbeaacc6d161c697d
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-26 19:10:34 +0200

    update navbar tests
    refactor code for readability, add tests for both unauthenticated and authenticated rendering/navigation

--- blog-frontend-author/__tests__/components/Navbar.test.tsx
+++ blog-frontend-author/__tests__/components/Navbar.test.tsx
@@ -1,26 +1,60 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { routes } from '../../src/App';
-import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+import {
+  createMemoryRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import AuthProvider from '../../src/provider/Provider';
 import { AuthContext } from '../../src/provider/context';
-import { TAuthContext } from '../../src/types/types';
+import { TAuthContext, TPostList } from '../../src/types/types';
+import userEvent from '@testing-library/user-event';
+import Posts from '../../src/pages/Posts';
+import HomePage from '../../src/pages/HomePage';
+import About from '../../src/pages/About';
+import Logout from '../../src/pages/Logout';
+import ErrorPage from '../../src/pages/ErrorPage';
+import GlobalLayout from '../../src/components/GlobalLayout';
 
-describe('navbar links conditional rendering', () => {
-  test('render Home, Signup, Login, About links if user is not authenticated', () => {
+describe('navbar component tests', () => {
+  const user = userEvent.setup();
+  describe('no auth rendering/navigating', () => {
     const router = createMemoryRouter(routes, { initialEntries: ['/home'] });
-    render(
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
-    );
-    expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: /signup/i })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: /login/i })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: /about/i })).toBeInTheDocument();
+    beforeEach(() => {
+      render(
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      );
+    });
+    test('render Home, Signup, Login, About links', () => {
+      expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: /signup/i })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: /login/i })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: /about/i })).toBeInTheDocument();
+    });
+    test('navigate to Home, Signup, Login, About pages', async () => {
+      await user.click(screen.getByRole('link', { name: /home/i }));
+      expect(
+        screen.getByRole('heading', { name: /homepage/i })
+      ).toBeInTheDocument();
+      await user.click(screen.getByRole('link', { name: /signup/i }));
+      expect(
+        screen.getByRole('heading', { name: /signup/i })
+      ).toBeInTheDocument();
+      await user.click(screen.getByRole('link', { name: /login/i }));
+      expect(
+        screen.getByRole('heading', { name: /login/i })
+      ).toBeInTheDocument();
+      await user.click(screen.getByRole('link', { name: /about/i }));
+      expect(
+        screen.getByRole('heading', { name: /about/i })
+      ).toBeInTheDocument();
+    });
   });
-  test('render Home, Posts, Logout, About links if user is authenticated', () => {
-    const router = createMemoryRouter(routes, { initialEntries: ['/home'] });
+  describe('auth rendering/navigating', () => {
     const testContext: TAuthContext = {
       authData: {
         token: 'test',
@@ -28,14 +62,54 @@ describe('navbar links conditional rendering', () => {
       },
       setAuthData: () => {},
     };
-    render(
-      <AuthContext.Provider value={testContext}>
-        <RouterProvider router={router} />
-      </AuthContext.Provider>
+    const noPosts: TPostList = [];
+    const testRoutes = createRoutesFromElements(
+      <Route path='/' errorElement={<ErrorPage />} element={<GlobalLayout />}>
+        <Route path='home' element={<HomePage />} />
+        <Route path='about' element={<About />} />
+        <Route path='logout' element={<Logout />} />
+        <Route
+          path='/posts'
+          loader={() => {
+            return { posts: noPosts };
+          }}
+          element={<Posts />}
+        />
+      </Route>
     );
-    expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: /posts/i })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: /logout/i })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: /about/i })).toBeInTheDocument();
+    const router = createMemoryRouter(testRoutes, {
+      initialEntries: ['/'],
+    });
+    beforeEach(() => {
+      render(
+        <AuthContext.Provider value={testContext}>
+          <RouterProvider router={router} />
+        </AuthContext.Provider>
+      );
+    });
+    test('render Home, Posts, Logout, About links', () => {
+      expect(screen.getByRole('link', { name: /home/i })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: /posts/i })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: /logout/i })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: /about/i })).toBeInTheDocument();
+    });
+    test('navigate to Home, Posts, Logout, About pages', async () => {
+      await user.click(screen.getByRole('link', { name: /home/i }));
+      expect(
+        screen.getByRole('heading', { name: /homepage/i })
+      ).toBeInTheDocument();
+      await user.click(screen.getByRole('link', { name: /posts/i }));
+      expect(
+        screen.getByRole('heading', { name: /Posts/ })
+      ).toBeInTheDocument();
+      await user.click(screen.getByRole('link', { name: /logout/i }));
+      expect(
+        screen.getByRole('heading', { name: /logging out/i })
+      ).toBeInTheDocument();
+      await user.click(screen.getByRole('link', { name: /about/i }));
+      expect(
+        screen.getByRole('heading', { name: /about/i })
+      ).toBeInTheDocument();
+    });
   });
 });
