f7c9fdaddac5c0aa3806d48c123dde3f157e51ed
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-28 10:44:56 +0200

    update Post tests

--- blog-frontend-author/__tests__/pages/Post.test.tsx
+++ blog-frontend-author/__tests__/pages/Post.test.tsx
@@ -10,12 +10,14 @@ import {
 import { AuthContext } from '../../src/provider/context';
 import { TAuthContext, TPostDetail } from '../../src/types/types';
 import Post from '../../src/pages/Post';
+import userEvent from '@testing-library/user-event';
 
-describe('post page rendering', () => {
+describe('post page tests', () => {
+  const user = userEvent.setup();
   const testContext: TAuthContext = {
     authData: {
       token: 'test',
-      user: { id: 'test', username: 'testUser', email: 'test@gmail.com' },
+      user: { id: '123', username: 'testUser', email: 'test@gmail.com' },
     },
     setAuthData: () => {},
   };
@@ -23,7 +25,7 @@ describe('post page rendering', () => {
     id: '1',
     title: 'testpost1',
     content: 'test content 1',
-    published: 'false',
+    published: false,
     createdAt: new Date('2024-09-18T09:56:31.751Z'),
     updatedAt: new Date('2024-09-18T09:56:31.751Z'),
     authorId: '123',
@@ -33,16 +35,12 @@ describe('post page rendering', () => {
     },
     comments: [],
   };
-
-  test('post details', async () => {
+  function getPost() {
+    return { post: postDetail };
+  }
+  beforeEach(() => {
     const testRoutes = createRoutesFromElements(
-      <Route
-        path='/posts/1'
-        element={<Post />}
-        loader={() => {
-          return { post: postDetail };
-        }}
-      />
+      <Route path='/posts/1' element={<Post />} loader={() => getPost()} />
     );
     const router = createMemoryRouter(testRoutes, {
       initialEntries: ['/posts/1'],
@@ -52,11 +50,17 @@ describe('post page rendering', () => {
         <RouterProvider router={router} />
       </AuthContext.Provider>
     );
+  });
+  test('post details display', async () => {
     expect(
       await screen.findByRole('heading', { name: 'testpost1' })
     ).toBeInTheDocument();
-    expect(screen.getByText('testusername')).toBeInTheDocument();
-    expect(screen.getAllByText('9/18/2024, 11:56:31 AM')).toBeDefined();
-    expect(screen.getByText('test content 1')).toBeInTheDocument();
+  });
+  test('post form display', async () => {
+    const editButton = await screen.findByRole('button', { name: /edit/i });
+    await user.click(editButton);
+    expect(
+      screen.getByRole('heading', { name: /edit post/i })
+    ).toBeInTheDocument();
   });
 });
