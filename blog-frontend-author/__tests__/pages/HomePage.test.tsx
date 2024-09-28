3ed0fc9e84218fb05960a8501f0c4d2c1422fb8f
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-09-28 09:55:02 +0200

    Update HomePage tests
    add links rendering/navigating tests

--- blog-frontend-author/__tests__/pages/HomePage.test.tsx
+++ blog-frontend-author/__tests__/pages/HomePage.test.tsx
@@ -1,27 +1,49 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { routes } from '../../src/App';
-import { createMemoryRouter, RouterProvider } from 'react-router-dom';
-
+import {
+  createMemoryRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import AuthProvider from '../../src/provider/Provider';
 import { AuthContext } from '../../src/provider/context';
-import { TAuthContext } from '../../src/types/types';
+import { TAuthContext, TPostList } from '../../src/types/types';
+import HomePage from '../../src/pages/HomePage';
+import Posts from '../../src/pages/Posts';
 
-describe('homepage conditional rendering', () => {
-  const router = createMemoryRouter(routes, { initialEntries: ['/home'] });
-  test('user not authenticated welcome message', () => {
-    render(
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
-    );
-    expect(
-      screen.getByText(
-        'Welcome to the homepage! On this site registered authors can manage both posts and comments.'
-      )
-    ).toBeInTheDocument();
+describe('HomePage tests', () => {
+  const user = userEvent.setup();
+  describe('no auth rendering/navigating', async () => {
+    beforeEach(() => {
+      const router = createMemoryRouter(routes, { initialEntries: ['/home'] });
+      render(
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      );
+    });
+    test('welcome message display', async () => {
+      expect(screen.getByText(/Welcome to the homepage/i)).toBeInTheDocument();
+    });
+    test('signup link', async () => {
+      const signupLink = screen.getByRole('link', { name: /sign up/ });
+      expect(signupLink).toBeInTheDocument();
+      await user.click(signupLink);
+      expect(screen.getByRole('heading', { name: /signup/i }));
+    });
+    test('login link', async () => {
+      const loginLink = screen.getByRole('link', { name: /log in/i });
+      expect(loginLink).toBeInTheDocument();
+      await user.click(loginLink);
+      expect(
+        screen.getByRole('heading', { name: /login/i })
+      ).toBeInTheDocument();
+    });
   });
-  test('user authenticated message', () => {
+  describe('auth rendering/navigating', () => {
     const testContext: TAuthContext = {
       authData: {
         token: 'test',
@@ -29,11 +51,37 @@ describe('homepage conditional rendering', () => {
       },
       setAuthData: () => {},
     };
-    render(
-      <AuthContext.Provider value={testContext}>
-        <RouterProvider router={router} />
-      </AuthContext.Provider>
-    );
-    expect(screen.getByText('testUser')).toBeInTheDocument();
+    const noPosts: TPostList = [];
+    function getPosts() {
+      return { posts: noPosts };
+    }
+    beforeEach(() => {
+      const testRoutes = createRoutesFromElements(
+        <>
+          <Route path='home' element={<HomePage />} />
+          <Route path='/posts' loader={() => getPosts()} element={<Posts />} />
+        </>
+      );
+      const router = createMemoryRouter(testRoutes, {
+        initialEntries: ['/home'],
+      });
+      render(
+        <AuthContext.Provider value={testContext}>
+          <RouterProvider router={router} />
+        </AuthContext.Provider>
+      );
+    });
+    test('user authenticated message', () => {
+      expect(screen.getByText(/testuser/i)).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: /post/ })).toBeInTheDocument();
+    });
+    test('posts link', async () => {
+      const postsLink = screen.getByRole('link', { name: /post/i });
+      expect(postsLink).toBeInTheDocument();
+      await user.click(postsLink);
+      expect(
+        screen.getByRole('heading', { name: 'Posts' })
+      ).toBeInTheDocument();
+    });
   });
 });
