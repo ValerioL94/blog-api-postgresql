f24ac77e6b25ce779443697dc55458df33b8714e
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-10-03 11:04:02 +0200

    update frontend-author posts tests

--- blog-frontend-author/__tests__/pages/Posts.test.tsx
+++ blog-frontend-author/__tests__/pages/Posts.test.tsx
@@ -9,11 +9,10 @@ import {
 import userEvent from '@testing-library/user-event';
 
 import { AuthContext } from '../../src/provider/context';
-import { TAuthContext, TPostDetail, TPostList } from '../../src/types/types';
+import { TAuthContext, TPostList } from '../../src/types/types';
 import Posts from '../../src/pages/Posts';
 import HomePage from '../../src/pages/HomePage';
 import ProtectedRoute from '../../src/pages/ProtectedRoute';
-import Post from '../../src/pages/Post';
 import NewPost from '../../src/pages/NewPost';
 
 describe('posts page test', () => {
@@ -62,23 +61,6 @@ describe('posts page test', () => {
   const noPosts: TPostList = [];
   function getNoPosts() {
     return { posts: noPosts };
-  }
-  const postDetail: TPostDetail = {
-    id: '1',
-    title: 'testpost1',
-    content: 'test content 1',
-    published: false,
-    createdAt: new Date('2024-09-18T09:56:31.751Z'),
-    updatedAt: new Date('2024-09-18T09:56:31.751Z'),
-    authorId: '123',
-    author: {
-      username: 'testusername',
-      email: 'testemail@gmail.com',
-    },
-    comments: [],
-  };
-  function getPost() {
-    return { post: postDetail };
   }
   describe('posts page conditional rendering', () => {
     test('render posts page if user is authenticated', async () => {
@@ -168,29 +150,8 @@ describe('posts page test', () => {
       <>
         <Route path='/posts' loader={() => getPosts()} element={<Posts />} />
         <Route path='/posts/new-post' element={<NewPost />} />
-        <Route
-          path='/posts/:postId'
-          element={<Post />}
-          loader={() => getPost()}
-        />
       </>
     );
-    test('navigate to specified post page', async () => {
-      const router = createMemoryRouter(testRoutes, {
-        initialEntries: ['/posts'],
-      });
-      render(
-        <AuthContext.Provider value={testContext}>
-          <RouterProvider router={router} />
-        </AuthContext.Provider>
-      );
-      const testLink = await screen.findByRole('link', { name: 'testpost1' });
-
-      await user.click(testLink);
-      expect(
-        await screen.findByRole('heading', { name: 'testpost1' })
-      ).toBeInTheDocument();
-    });
     test('navigate to new post page', async () => {
       const router = createMemoryRouter(testRoutes, {
         initialEntries: ['/posts'],
