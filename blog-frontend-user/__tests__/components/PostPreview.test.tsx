bc12cae42920f407962933638024d2f50a4a8152
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-10-03 12:13:11 +0200

    add frontend-user PostPreview tests

---
+++ blog-frontend-user/__tests__/components/PostPreview.test.tsx
@@ -0,0 +1,83 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import {
+  createMemoryRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+
+import { TPostDetail, TPostList } from '../../src/types/types';
+import Posts from '../../src/pages/Posts';
+import Post from '../../src/pages/Post';
+
+describe('postPreview tests', () => {
+  const user = userEvent.setup();
+  const testPosts: TPostList = [
+    {
+      id: '1',
+      title: 'testpost1',
+      content: 'test content 1',
+      published: false,
+      createdAt: new Date('2024-09-18T09:56:31.751Z'),
+      updatedAt: new Date('2024-09-18T09:59:31.751Z'),
+      authorId: '123',
+      author: {
+        username: 'testusername',
+        email: 'testemail@gmail.com',
+      },
+    },
+  ];
+  function getPosts() {
+    return { posts: testPosts };
+  }
+  const postDetail: TPostDetail = {
+    id: '1',
+    title: 'testpost1',
+    content: 'test content 1',
+    published: false,
+    createdAt: new Date('2024-09-18T09:56:31.751Z'),
+    updatedAt: new Date('2024-09-18T09:56:31.751Z'),
+    authorId: '123',
+    author: {
+      username: 'testusername',
+      email: 'testemail@gmail.com',
+    },
+    comments: [],
+  };
+  function getPost() {
+    return { post: postDetail };
+  }
+  beforeEach(() => {
+    const testRoutes = createRoutesFromElements(
+      <>
+        <Route path='/posts' loader={() => getPosts()} element={<Posts />} />
+        <Route
+          path='/posts/:postId'
+          element={<Post />}
+          loader={() => getPost()}
+        />
+      </>
+    );
+    const router = createMemoryRouter(testRoutes, {
+      initialEntries: ['/posts'],
+    });
+    render(<RouterProvider router={router} />);
+  });
+  test('post preview renders correctly', async () => {
+    expect(
+      await screen.findByRole('heading', { name: 'testpost1' })
+    ).toBeInTheDocument();
+    expect(screen.getByText('testusername')).toBeInTheDocument();
+    expect(screen.getByText('9/18/2024, 11:56:31 AM')).toBeInTheDocument();
+    expect(screen.getByText('9/18/2024, 11:59:31 AM')).toBeInTheDocument();
+  });
+  test('navigate to specified post page', async () => {
+    const postLink = await screen.findByRole('link', { name: 'testpost1' });
+    await user.click(postLink);
+    expect(
+      await screen.findByRole('heading', { name: 'testpost1' })
+    ).toBeInTheDocument();
+  });
+});
