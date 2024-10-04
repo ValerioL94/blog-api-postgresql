68afd1a39bdb37e3706a575a5f47f87521b47072
ValerioL94 <valerio.lemma1994@gmail.com>
2024-10-04 14:27:31 +0200

    Update vite.config.ts

--- blog-frontend-author/vite.config.ts
+++ blog-frontend-author/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://blog-api-backend-eoez.onrender.com',
+        target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
